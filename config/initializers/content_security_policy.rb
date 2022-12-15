0ec2561a9fa85f28048ac3dcee4ba8f42e33bf64
Sophy Lee <stlee26@gmail.com>
2022-12-14 18:48:21 -0600

    Add content security policy

---
+++ config/initializers/content_security_policy.rb
@@ -0,0 +1,11 @@
+Rails.application.config.content_security_policy do |policy|
+  policy.default_src :self, :https
+  policy.font_src    :self, :https, :data
+  policy.img_src     :self, :https, :data
+  policy.object_src  :none
+  policy.script_src  :self, :https
+  policy.style_src   :self, :https
+  # TODO: update the uri below
+  # Specify URI for violation reports
+  policy.report_uri "/csp-violation-report-endpoint"
+end
