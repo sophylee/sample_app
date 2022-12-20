d7b73ca8ecd9d03248aeb04522c36852098a52fa
Sophy Lee <stlee26@gmail.com>
2022-12-20 10:37:02 -0600

    Update tests

--- spec/requests/csrf_security_spec.rb
+++ spec/requests/csrf_security_spec.rb
@@ -39,78 +39,50 @@ describe 'CSRF security', type: :request do
         post user_registration_url, params: params, headers: with_pre_session_token_headers
         expect(response.status).to eq(201)
 
-        # Get the new CSRF token returned by the API to the client. Note that this token is a masked, 
-        # encrypted version of the same real CSRF token as the pre-session token. In other words:
+        # Get the new CSRF token returned by the API to the client. This token should be a masked, 
+        # encrypted version of a _different_ CSRF token than the one for the pre-session token. 
+        # In other words:
         # 
-        # unmask_token(decode_csrf_token(pre_session_csrf_token)) == unmask_token(decode_csrf_token(session_csrf_token))
+        # unmask_token(decode_csrf_token(pre_session_csrf_token)) != unmask_token(decode_csrf_token(session_csrf_token))
         # 
-        # This is because .real_csrf_token in ActionController retrieves the same CSRF token for the pre-auth session 
-        # as it does for the post-auth session. This CSRF token is then used in .valid_authenticity_token? to figure out 
-        # the global CSRF token for this session. In other words: 
+        # This is important because .real_csrf_token in ActionController decodes and unmasks the client-facing 
+        # CSRF token, then uses .valid_authenticity_token? to compare the token to the global CSRF token for this session. 
+        # The global CSRF token for the anonymous session should be different than the global CSRF token for 
+        # the authenticated session. Otherwise, someone with the anonymous session CSRF token would be able to 
+        # execute a CSRF token fixation attack. In other words: 
         #
-        # REAL pre-session CSRF token == REAL session CSRF token
+        # REAL pre-session CSRF token != REAL session CSRF token
         # 
-        # which means that: global_csrf_token(pre_session) == global_csrf_token(session)
+        # which means that: global_csrf_token(pre_session) != global_csrf_token(session)
         # 
         # which means that: sending the pre_session X-CSRF-TOKEN header in an authenticated request 
-        # works, since ActionController converts this header into a real CSRF token, which is matched 
-        # against the global CSRF token for the session, and the two are the same. In other words: 
-        # 
-        # unmask_token(decode_csrf_token(pre_session_csrf_token)) ==
-        #   unmask_token(decode_csrf_token(session_csrf_token)) ==
-        #   real_csrf_token(pre_session) == 
-        #   real_csrf_token(session) == 
-        #   global_csrf_token(pre_session) ==
-        #   global_csrf_token(session)
-        # 
-        # This is pretty counter-intuitive, since I would have thought that the real CSRF token changes
-        # once you authenticate, but this is not the case.
+        # should NOT work, since ActionController converts this header into a real CSRF token, which is
+        # matched against the global CSRF token for the session, and the two are NOT the same. 
         # 
         # All ActionController methods in the above explanation can be found here: 
         # https://github.com/rails/rails/blob/main/actionpack/lib/action_controller/metal/request_forgery_protection.rb
         session_csrf_token = response.header["Set-Cookie"].split(";")[0].split("_api_csrf_token=")[1]
         
-        # Since the tokens are masked and encoded, we expect them to look **different** even though they're the same
-        # underlying CSRF token.
+        # Since the tokens are masked and encoded, we expect them to look **different**. This doesn't prove yet that the
+        # real CSRF token underlying these client-facing tokens are different. We'll prove that later.
         expect(pre_session_csrf_token).to_not eq(session_csrf_token)
 
         # Decode and then unmask the pre_session_csrf_token and session_csrf_token to get their real values
         # The unmasking and decoding code are pulled straight from ActionController RequestForgeryProtection.
-        masked_pre_session_csrf_token = Base64.urlsafe_decode64(pre_session_csrf_token)
-        s1 = masked_pre_session_csrf_token[0...32]
-        s2 = masked_pre_session_csrf_token[32..-1]
-        s2 = s2.dup
-        size = s1.bytesize
-        i = 0
-        while i < size
-          s2.setbyte(i, s1.getbyte(i) ^ s2.getbyte(i))
-          i += 1
-        end
-        real_pre_session_csrf_token = s2
+        decoded_pre_session_csrf_token = Base64.urlsafe_decode64(pre_session_csrf_token)
+        real_pre_session_csrf_token = unmask(decoded_pre_session_csrf_token)
 
-        # Get the real session CSRF token
-        masked_session_csrf_token = Base64.urlsafe_decode64(session_csrf_token)
-        s3 = masked_session_csrf_token[0...32]
-        s4 = masked_session_csrf_token[32..-1]
-        s4 = s4.dup
-        size = s3.bytesize
-        i = 0
-        while i < size
-          s4.setbyte(i, s3.getbyte(i) ^ s4.getbyte(i))
-          i += 1
-        end
-        real_session_token = s4
+        decoded_session_csrf_token = Base64.urlsafe_decode64(session_csrf_token)
+        real_session_csrf_token = unmask(decoded_session_csrf_token)
 
-        expect(real_pre_session_csrf_token).to_not eq(real_session_token)
+        expect(real_pre_session_csrf_token).to_not eq(real_session_csrf_token)
 
-        # Now try to make an authenticated request using the pre_session_csrf_token. It should still work.
-        # We can use the same headers and params as we used for registration. TODO test a real endpoint here.
+        # Now try to make an authenticated request using the pre_session_csrf_token. It should NOT work.
+        # We can use the same headers and params as we used for registration. 
         post authenticated_endpoint_api_v1_users_url, params: params, headers: with_pre_session_token_headers
         expect(response.status).to eq(403)
-        # expect(JSON.parse(response.body)["data"]["attributes"]).to eq({"email" => email})
-        # expect(response.headers["Set-Cookie"].include?("_api_csrf_token=")).to eq(true)
 
-        # Now make a request using the session_token. It should work too.
+        # Now make a request using the session_token. It SHOULD work.
         with_session_token_headers = { 'Content-Type' => 'application/json', 'X-CSRF-Token' => session_csrf_token }
         post authenticated_endpoint_api_v1_users_url, params: params, headers: with_session_token_headers
         expect(response.status).to eq(200)
@@ -118,8 +90,8 @@ describe 'CSRF security', type: :request do
         expect(response.headers["Set-Cookie"].include?("_api_csrf_token=")).to eq(true)
         new_token = response.header["Set-Cookie"].split(";")[0].split("_api_csrf_token=")[1]
 
-        # Sign out the user using the pre_session_csrf_token
-        with_new_session_token_headers = { 'Content-Type' => 'application/json', 'X-CSRF-Token' => new_token }
+        # Sign out the user using the session_csrf_token
+        with_new_session_token_headers = { 'Content-Type' => 'application/json', 'X-CSRF-Token' => session_csrf_token }
         delete destroy_user_session_url, headers: with_new_session_token_headers
         expect(response.status).to eq(204)
         expect(response.headers["Set-Cookie"].include?("_api_csrf_token=")).to eq(true)
@@ -145,6 +117,19 @@ describe 'CSRF security', type: :request do
         expect(JSON.parse(response.body)["email"]).to eq(email) # TODO update devise sessions controller to use json serializer
         expect(response.headers["Set-Cookie"].include?("_api_csrf_token=")).to eq(true)
       end
+    end
+
+    def unmask(decoded_csrf_token)
+      s1 = decoded_csrf_token[0...32]
+      s2 = decoded_csrf_token[32..-1]
+      s2 = s2.dup
+      size = s1.bytesize
+      i = 0
+      while i < size
+        s2.setbyte(i, s1.getbyte(i) ^ s2.getbyte(i))
+        i += 1
+      end
+      return s2
     end
   end  
 end
